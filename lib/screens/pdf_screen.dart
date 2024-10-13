bc97f2427b83bac95dee16f24db7c0dc3f10fa9d
Maham <mahaamimran@gmail.com>
2024-10-13 11:56:53 +0500

    corrected links

--- lib/screens/pdf_screen.dart
+++ lib/screens/pdf_screen.dart
@@ -9,7 +9,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
 import 'package:dei_marc/utils/connection_util.dart';
-import 'package:uuid/uuid.dart'; // Import the UUID package
+import 'package:uuid/uuid.dart'; 
 
 class PDFScreen extends StatefulWidget {
   final Color appBarColor;
